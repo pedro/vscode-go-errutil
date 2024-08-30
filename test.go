47545ba6a94e916d2a5f12d1a53ff9709ace001f
Pedro Belo <pedrobelo@gmail.com>
2024-08-29 22:15:05 -0700

    better test file

--- test.go
+++ test.go
@@ -2,10 +2,20 @@ package main
 
 func main() {
 	if err := x(); err != nil {
-
+		panic(err)
 	}
 }
 
-func x() error {
-	return nil
+type s struct{}
+
+func a() (n int, s string, p *s, err error) {
+	if err := x(); err != nil {
+		return 0, "", nil, errutil.With(err)
+	}
+
+	return 1, "a", &s{}, nil
+}
+
+func x() (int, error) {
+	return 0, nil
 }
